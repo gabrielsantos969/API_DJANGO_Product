b479ceee50d230f58b197dac153bacbf9aec6e82
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-12-20 23:09:46 -0300

    Criando serializers para receber dados do property de um model e adicionado o rest_framework nos settings

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,12 +5,14 @@ from products.models import Product
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from products.serializers import ProtuctSerializer
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data= model_to_dict(model_data, fields=['id', 'title', 'price'])
+    if instance:
+        # data= model_to_dict(instance, fields=['id', 'title', 'price'])
+        data = ProtuctSerializer(instance).data
     return Response(data)
    
---
+++ backend/products/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import Product
+
+class Protuct(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'content',
+            'price'
+        ]
