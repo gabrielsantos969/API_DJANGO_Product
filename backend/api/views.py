06ea4bd15f4a6c24400c177afd2e427afe775a07
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-12-21 00:00:27 -0300

    Feito um metodo POST

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,34 @@
 import json
 from django.forms import model_to_dict
-# from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse
 from products.models import Product
+
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from products.serializers import ProtuctSerializer
 
-@api_view(["GET"])
+@api_view(["POST"])
+def api_home(request, *args, **kwargs):
+
+    serializer = ProtuctSerializer(data=request.data)
+
+    if serializer.is_valid(raise_exception=True):
+        
+        #instance = serializer.save()
+        print(serializer.data)
+        return JsonResponse(serializer.data)
+
+    return Response({"invalid": "Not good data"}, status=400)
+
+
+
+""" @api_view(["GET"])
 def api_home(request, *args, **kwargs):
     instance = Product.objects.all().order_by("?").first()
     data = {}
     if instance:
         # data= model_to_dict(instance, fields=['id', 'title', 'price'])
         data = ProtuctSerializer(instance).data
-    return Response(data)
+    return Response(data) """
    
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,8 @@ import requests
 # endpoint  =  "https://httpbin.org/anything"
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World"})
+# get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World"})
+get_response = requests.post(endpoint, json={"title": "Produto Teste","content": "Hello World"})
 # print(get_response.headers)
 # print(get_response.text)
 print(get_response.json())
