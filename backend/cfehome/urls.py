518950aa2fb000fa060e4b15ec9ef85bd9cde7d8
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-12-21 00:25:59 -0300

    Criacao de arquivo detail.py, criacao de view de DetailAPIView e urls

--- backend/cfehome/urls.py
+++ backend/cfehome/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/products/', include('products.urls')),
 
 ]
